49d3370f932fd250015a71ce249093511b941a4e
Behrooz Sepehry <behrooz.sepehry@gmail.com>
2019-01-20 16:58:44 -0800

    use the same cnn as gan for vae

--- models/gan/mnist_cnn_1.py
+++ models/gan/mnist_cnn_1.py
@@ -1,10 +1,10 @@
 from models.gan import base
-from models import cnn_conv_1, cnn_deconv_1
+from models import mnist_conv_1, mnist_deconv_1
 
 
 class Model(base.GanModelBase):
     def __init__(self, *args, **kwargs):
         super(Model, self).__init__(*args, **kwargs)
         mid_channels = kwargs.get('mid_channels', 1)
-        self.discriminator = cnn_conv_1.Model(1, mid_channels, 1, activation='Sigmoid')
-        self.generator = cnn_deconv_1.Model(self.z_args['z_dim'], mid_channels, 1, activation='Sigmoid')
+        self.discriminator = mnist_conv_1.Model(1, mid_channels, 1, activation='Sigmoid')
+        self.generator = mnist_deconv_1.Model(self.z_args['z_dim'], mid_channels, 1, activation='Sigmoid')
--- models/vae/mnist_cnn_normal_1.py
+++ models/vae/mnist_cnn_normal_1.py
@@ -2,61 +2,25 @@ import torch
 from torch import nn
 
 from models.vae import normal_base
+from models import mnist_conv_1, mnist_deconv_1
 
 
 class Model(normal_base.VaeModelNormalBase):
     def __init__(self, *args, **kwargs):
         super(Model, self).__init__(*args, **kwargs)
-
-        self.conv = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=1, padding=0),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),
-            nn.Conv2d(16, 32, 3, stride=1, padding=0),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),
-            nn.Conv2d(32, 32, 3, stride=1, padding=0),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),
-            nn.Conv2d(32, 32, 3, stride=1, padding=0),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),
-            nn.Conv2d(32, 16, 3, stride=1, padding=0),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),
-        )
-
+        mid_channels = kwargs.get('mid_channels', 1)
         z_dim = self.z_args['z_dim']
-        self.fc11 = nn.Linear(16 * 13 * 13, z_dim)
-        self.fc12 = nn.Linear(16 * 13 * 13, z_dim)
-
-        self.fc2 = nn.Sequential(nn.Linear(z_dim, 7 * 7 * 32),
-                                 nn.ReLU(True),
-                                 )
-
-        self.deconv = nn.Sequential(
-            nn.ConvTranspose2d(32, 32, 3, stride=1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32, 32, 3, stride=1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32, 32, 3, stride=1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32, 16, 3, stride=1),
-            nn.ReLU(True),
-        )
-        self.deconv_mu = nn.Sequential(nn.ConvTranspose2d(16, 1, 4, stride=2, padding=2),
-            nn.Sigmoid(),
-        )
-        self.deconv_logvar = nn.Sequential(nn.ConvTranspose2d(16, 1, 4, stride=2, padding=2),
-        )
+        self.encoder = mnist_conv_1.Model(1, mid_channels, 2 * z_dim)
+        self.decoder = mnist_deconv_1.Model(z_dim, mid_channels, 2)
 
     def encode(self, x, **kwargs):
-        h1 = self.conv(x).view(x.size(0), -1)
-        z_mu, z_logvar = self.fc11(h1), self.fc12(h1)
+        zz = self.encoder(x)
+        z_mu, z_logvar = torch.split(zz, zz.size(1)//2, dim=1)
         return dict(z_mu=z_mu, z_logvar=z_logvar)
 
     def _decode(self, z, **kwargs):
-        h2 = self.fc2(z).view(z.size(0), 32, 7, 7)
-        h3 = self.deconv(h2)
-        x_mu, x_logvar = self.deconv_mu(h3), torch.clamp(self.deconv_logvar(h3), min=-10.)
+        xx = self.decoder(z)
+        x_mu, x_logvar = torch.split(xx, xx.size(1)//2, dim=1)
+        x_mu = torch.sigmoid(x_mu)
+        x_logvar = torch.clamp(x_logvar, min=-10.)
         return dict(x_mu=x_mu, x_logvar=x_logvar)
