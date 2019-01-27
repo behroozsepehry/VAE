d1e1f8acedfba82c7612c2f54934875110362b2d
Behrooz Sepehry <behrooz.sepehry@gmail.com>
2019-01-26 17:15:41 -0800

    add tresholding sparsity

---
+++ models/vae/mnist_cnn_normal_1_sparse_2.py
@@ -0,0 +1,5 @@
+from models.vae import normal_sparse_base_2, mnist_cnn_normal_1
+
+
+class Model(normal_sparse_base_2.NormalSparseBase2, mnist_cnn_normal_1.Model):
+    pass
---
+++ models/vae/normal_sparse_base_2.py
@@ -0,0 +1,18 @@
+import torch
+
+from models.vae import normal_sparse_base
+
+
+class NormalSparseBase2(normal_sparse_base.NormalSparseBase):
+    def __init__(self, *args, **kwargs):
+        super(NormalSparseBase2, self).__init__(*args, **kwargs)
+
+    def _denoise(self, z, **kwargs):
+        h1 = self.fc_d_1(z)
+        if self.training:
+            z_2 = h1
+        else:
+            z_2 = torch.where(h1 > self.z_args['threshold'], h1, torch.tensor([0.], device=h1.device))
+        z_d = self.fc_d_2(z_2)
+        return dict(z_d=z_d, z_2=z_2)
+
