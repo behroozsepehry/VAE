710df1e83ff1617979b0242b9bab1ffe2666f725
Behrooz Sepehry <behrooz.sepehry@gmail.com>
2019-01-26 21:27:47 -0800

    add gridsearch hypertuning with cross validation

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
 import argparse
 import yaml
+import itertools
+import copy
 import numpy as np
 
 import torch
 
 from utilities import main_utilities as mu
+from utilities import general_utilities as gu
 
 
-def construct_objects(**kwargs):
-    seed = kwargs.get('seed')
+def construct_objects(settings):
+    seed = settings.get('seed')
     if seed is not None:
         torch.manual_seed(seed)
 
-    device = mu.get_device(**kwargs['Device'])
-    model = mu.get_model(**kwargs['Model'])
+    device = mu.get_device(**settings['Device'])
+    model = mu.get_model(**settings['Model'])
     model.load(map_location=device)
     model = model.to(device)
-    dataloaders = mu.get_dataloaders(**kwargs['Dataloaders'])
+    dataloaders = mu.get_dataloaders(**settings['Dataloaders'])
     model_parameters_groups = model.get_parameters_groups()
-    optimizers = mu.get_optimizers(model_parameters_groups, **kwargs['Optimizers'])
-    losses = mu.get_losses(**kwargs['Losses'])
+    optimizers = mu.get_optimizers(model_parameters_groups, **settings['Optimizers'])
+    losses = mu.get_losses(**settings['Losses'])
     assert losses.keys() == optimizers.keys()
-    logger = mu.get_logger(**kwargs['Logger'])
+    logger = mu.get_logger(**settings['Logger'])
 
     if logger and logger.flags.get('conf'):
-        logger.add_text('conf/conf', str(kwargs))
+        logger.add_text('conf/conf', str(settings))
 
     return device, model, dataloaders, optimizers, losses, logger
+
+
+def train(settings):
+    objects = construct_objects(settings)
+    device, model, dataloaders, optimizers, losses, logger = objects
+    result = model.train_model(device, dataloaders, optimizers, losses, logger)
+    if logger:
+        logger.close()
+    return result
+
+
+def hypertune(settings):
+    hypertune_settings = settings.get('Hypertune', {})
+    hypertune_params_settings = hypertune_settings.get('params', {})
+    hypertune_params_settings_list_sizes = gu.get_list_size_of_dict_of_lists(hypertune_params_settings)
+    hypertune_params_settings_list_sizes_lists = [range(s) for s in hypertune_params_settings_list_sizes]
+    all_possible_choices = itertools.product(*hypertune_params_settings_list_sizes_lists)
+
+    best_choice = None
+    best_val = np.inf
+    for choice in all_possible_choices:
+        train_settings = copy.deepcopy(settings)
+        gu.choose_1_from_dict_of_lists(hypertune_params_settings, choice, train_settings)
+        val = train(train_settings)
+        if val < best_val:
+            best_val = val
+            best_choice = copy.deepcopy(choice)
+
+    best_hyperparams = copy.deepcopy(hypertune_params_settings)
+    gu.choose_1_from_dict_of_lists(hypertune_params_settings, best_choice, best_hyperparams)
+
+    return dict(best_hyperparams=best_hyperparams, best_val=best_val)
 
 
 def main():
@@ -35,12 +70,14 @@ def main():
                         help='configuration file path')
     args = parser.parse_args()
     with open(args.conf_path, 'rb') as f:
-        kwargs = yaml.load(f)
-        objects = construct_objects(**kwargs)
-        device, model, dataloaders, optimizers, losses, logger = objects
-        model.train_model(device, dataloaders, optimizers, losses, logger)
-        if logger:
-            logger.close()
+        settings = yaml.load(f)
+
+    if settings['function'] == 'train':
+        result = train(settings)
+    elif settings['function'] == 'hypertune':
+        result = hypertune(settings)
+
+    print('result: %s' % (result))
 
 
 if __name__ == '__main__':
--- utilities/general_utilities.py
+++ utilities/general_utilities.py
@@ -10,3 +10,40 @@ def import_from_path(path):
 
 def append_key_dict(d, k):
     return {k+kk: vv for kk, vv in d.items()}
+
+
+def choose_1_from_dict_of_lists(dict_of_lists, choice, result, i_choice=0):
+    """
+    :param dict_of_lists: {a: {b:[1,2,3], c:[11,22]}, d:{e:{f:[111, 222]}}}
+    :param choice: [2,1,2]
+    :param result: must have all the keys in dict_of_lists
+    :param i_choice: the index on the choices
+    :return: {a: {b: 2, c:11}, d:{e:{f:222}}}
+    """
+
+    for k in sorted(dict_of_lists.keys()):
+        dtype = type(dict_of_lists[k])
+        if dtype is dict:
+            i_choice = choose_1_from_dict_of_lists(dict_of_lists[k], choice, result[k], i_choice)
+        elif dtype is list:
+            result[k] = dict_of_lists[k][choice[i_choice]]
+            i_choice += 1
+        else:
+            raise NotImplementedError
+    return i_choice
+
+
+def __get_list_size_of_dict_of_lists(dict_of_lists, result):
+    for k in sorted(dict_of_lists.keys()):
+        dtype = type(dict_of_lists[k])
+        if dtype is dict:
+            __get_list_size_of_dict_of_lists(dict_of_lists[k], result)
+        elif dtype is list:
+            result.append(len(dict_of_lists[k]))
+        else:
+            raise NotImplementedError
+    return result
+
+
+def get_list_size_of_dict_of_lists(dict_of_lists):
+    return __get_list_size_of_dict_of_lists(dict_of_lists, [])
