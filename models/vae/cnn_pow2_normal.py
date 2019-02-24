72eab247dc10391154c4dbef8709b484ff3e3477
Behrooz Sepehry <behrooz.sepehry@gmail.com>
2019-02-24 12:31:50 -0800

    replace cnn 32 & 64 with power 2

--- models/vae/cnn_pow2_normal.py
+++ models/vae/cnn_pow2_normal.py
@@ -1,5 +1,5 @@
 from models.vae import base_nn_normal
-from models import conv_64, deconv_64
+from models import conv_pow2, deconv_pow2
 
 
 class Model(base_nn_normal.Model):
@@ -7,7 +7,8 @@ class Model(base_nn_normal.Model):
         super(Model, self).__init__(*args, **kwargs)
         mid_channels = kwargs.get('mid_channels', 1)
         in_channels = kwargs.get('in_channels', 1)
+        in_size = kwargs['in_size']
         z_dim = self.z_args['z_dim']
 
-        self.encoder = conv_64.Model(in_channels, mid_channels, 2 * z_dim)
-        self.decoder = deconv_64.Model(z_dim, mid_channels, 2 * in_channels)
+        self.encoder = conv_pow2.Model(in_size, in_channels, mid_channels, 2 * z_dim)
+        self.decoder = deconv_pow2.Model(in_size, z_dim, mid_channels, 2 * in_channels)
